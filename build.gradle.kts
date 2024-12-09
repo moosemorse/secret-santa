1a0c9cb6dca68f61f804776a61cfa9ab6d1ea526
Alex <aivaylovivanov@gmail.com>
2024-12-09 10:35:47 +0000

    update config spec for apis/dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,11 +8,34 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    google()
+    maven { url = uri("https://dl.google.com/dl/android/maven2") }
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
-    testImplementation(kotlin("test"))
+    // https://mvnrepository.com/artifact/com.google.api/gax-grpc
+    implementation("com.google.api:gax-grpc:2.58.0")
+    implementation("javax.mail:javax.mail-api:1.6.2")
+    implementation("com.sun.mail:javax.mail:1.6.2")
+    // https://mvnrepository.com/artifact/com.google.api-client/google-api-client-java6
+    implementation("com.google.api-client:google-api-client-java6:2.1.4")
+    // https://mvnrepository.com/artifact/com.google.api-client/google-api-client-jackson2
+    implementation("com.google.api-client:google-api-client-jackson2:2.7.0")
+    implementation("com.google.apis:google-api-services-tasks:v1-rev71-1.25.0")
+    implementation("com.google.oauth-client:google-oauth-client:1.33.3")
+    implementation("com.google.http-client:google-http-client-gson:1.40.1")
+    // https://mvnrepository.com/artifact/com.google.api-client/google-api-client
+    implementation("com.google.api-client:google-api-client:2.7.0")
+    // https://mvnrepository.com/artifact/com.google.apis/google-api-services-gmail
+    implementation("com.google.apis:google-api-services-gmail:v1-rev110-1.25.0")
+    // https://mvnrepository.com/artifact/com.google.api-client/google-api-client-appengine
+    implementation("com.google.api-client:google-api-client-appengine:2.7.0")
+
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
 }
 
 application {
